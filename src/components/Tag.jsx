43a1894bf034e6f4cc63e1410a0e5fe8d0e62a07
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-10-03 21:56:54 +0200

    Visual toggle of the tags

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,19 +1,24 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { styled } from 'styled-components'
 import { TagContext } from '../App';
 
 export const Tag = ({ name }) => {
   const addTag = useContext(TagContext);
+  const [selected, setSelected] = useState(false);
+
+  const handleTagToggle = (name) => {
+    addTag(name);
+    setSelected(!selected);
+  }
 
   return (
-    <Container onClick={() => addTag(name)} >{name}</Container>
+    <Container onClick={() => handleTagToggle(name)} className={selected ? 'selected' : ''} >{name}</Container>
   )
 }
 
 const Container = styled.div`
   user-select: none;
   cursor: pointer;
-  /* background-color: #223d66; */
   border: 2px solid rgba(0, 0, 0, .25);
   color: #4ab9ec;
   color: rgba(0, 0, 0, .5);
@@ -27,6 +32,12 @@ const Container = styled.div`
   font-weight: 500;
 
   &:hover {
+    border: 2px solid rgba(0, 0, 0, 1);
+    color: rgba(0, 0, 0, 1);
+    transition: all .25s ease-out;
+  }
+
+  &.selected {
     border: 2px solid rgba(0, 0, 0, 1);
     color: rgba(0, 0, 0, 1);
     transition: all .25s ease-out;
